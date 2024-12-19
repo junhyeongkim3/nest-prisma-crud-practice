0bc747e853cfcb9e6af9ee2fa610227ff96921fb
junhyeongkim3 <kim.junhyeong@linkareer.com>
2024-12-19 17:33:48 +0900

    feat(quizzes): quizzes 서비스 컨트롤러 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WordsModule } from './words/words.module';
+import { QuizzesModule } from './quizzes/quizzes.module';
 
 @Module({
-  imports: [WordsModule],
+  imports: [WordsModule, QuizzesModule],
   controllers: [AppController],
   providers: [AppService],
 })
