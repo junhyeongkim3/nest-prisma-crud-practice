3f933de7cbd4a0c08be419167b5c256dbf818964
junhyeongkim3 <kim.junhyeong@linkareer.com>
2024-12-19 17:43:32 +0900

    feat(guard): admin guard 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WordsModule } from './words/words.module';
 import { QuizzesModule } from './quizzes/quizzes.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [WordsModule, QuizzesModule],
+  imports: [WordsModule, QuizzesModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
